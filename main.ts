7b1dd5bd70f330971b904dd52400905384840245
richardlisha <113388339+richardlisha@users.noreply.github.com>
2022-10-14 18:47:53 -0700

    boat rotates

--- main.ts
+++ main.ts
@@ -1,22 +1,54 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    turnBoat(currentBoat)
+})
+function makeBoatVisible (boatarray: Sprite[]) {
+    for (let value of boatarray) {
+        value.setFlag(SpriteFlag.Invisible, false)
+    }
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (currentBoat == 2) {
+        cursor.setFlag(SpriteFlag.Invisible, false)
+        moveBoatFlag = 0
+    } else {
+        currentBoat += 1
+    }
+})
 function moveBoat (boatArray: Sprite[]) {
-    if (grid.spriteRow(cursor) == 6) {
+    makeBoatVisible(boatarray)
+    if (grid.spriteRow(cursor) == 6 && boatRotateArray[currentBoat] == "up") {
         grid.move(cursor, 0, -1)
+    }
+    if (grid.spriteCol(cursor) == 8 && boatRotateArray[currentBoat] == "sideways") {
+        grid.move(cursor, -1, 0)
     }
     cursor.setFlag(SpriteFlag.Invisible, true)
     iterator = 0
     for (let value of boatArray) {
-        grid.place(value, tiles.getTileLocation(grid.spriteCol(cursor), grid.spriteRow(cursor) + iterator))
+        if (boatRotateArray[currentBoat] == "up") {
+            grid.place(value, tiles.getTileLocation(grid.spriteCol(cursor), grid.spriteRow(cursor) + iterator))
+        } else {
+            grid.place(value, tiles.getTileLocation(grid.spriteCol(cursor) + iterator, grid.spriteRow(cursor)))
+        }
         iterator += 1
     }
-    if (controller.A.isPressed()) {
-        cursor.setFlag(SpriteFlag.Invisible, false)
-        moveBoatFlag = 0
+}
+function makeBoatInvisible (boatarray: Sprite[]) {
+    for (let value of boatarray) {
+        value.setFlag(SpriteFlag.Invisible, true)
     }
+}
+function turnBoat (boatNum: any[]) {
+	
 }
 let iterator = 0
 let cursor: Sprite = null
 let moveBoatFlag = 0
-let boatRotateArray = ["up", "up", "up"]
+let boatarray: number[] = []
+let boatRotateArray: string[] = []
+let currentBoat = 0
+currentBoat = 0
+boatRotateArray = ["up", "up", "up"]
 let boatSpriteArray = [[sprites.create(img`
     . . . . . b b b b b b . . . . . 
     . . . b b 9 9 9 9 9 9 b b . . . 
@@ -103,6 +135,10 @@ let boatSpriteArray = [[sprites.create(img`
     . . . b b 5 5 5 5 5 5 b b . . . 
     . . . . . b b b b b b . . . . . 
     `, SpriteKind.Player)]]
+for (let value of boatarray) {
+    let list: number[] = []
+    makeBoatInvisible(list)
+}
 moveBoatFlag = 1
 tiles.setCurrentTilemap(tilemap`level2`)
 cursor = sprites.create(img`
@@ -127,7 +163,6 @@ grid.snap(cursor)
 grid.moveWithButtons(cursor)
 game.onUpdate(function () {
     if (moveBoatFlag == 1) {
-        let currentBoat = 0
         moveBoat(boatSpriteArray[currentBoat])
     }
 })
